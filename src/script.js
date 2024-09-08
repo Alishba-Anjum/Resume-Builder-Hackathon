3b7239d4a675895e81ba20b8277c495d750f0920
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2024-09-08 12:21:27 +0500

    milestone 04 05

--- src/script.js
+++ src/script.js
@@ -13,3 +13,69 @@ skillsBtn === null || skillsBtn === void 0 ? void 0 : skillsBtn.addEventListener
         showSkills = true;
     }
 });
+document.addEventListener("DOMContentLoaded", function () {
+    var userName = document.querySelector("#name");
+    var userEmail = document.querySelector("#email");
+    var userContact = document.querySelector("#number");
+    var userExperience = document.querySelector("#experience");
+    var userEducation = document.querySelector("#education");
+    var userSkills = document.querySelector("#skills");
+    var generateBtn = document.querySelector("button");
+    var userNameResume = document.querySelector("#userNameresume");
+    var userContactResume = document.querySelector("#userContactresume");
+    var userEmailResume = document.querySelector("#userEmailresume");
+    var userEducationResume = document.querySelector("#userEducationresume");
+    var userExperienceResume = document.querySelector("#userExperienceresume");
+    var userSkillsResume = document.querySelector("#userSkillsresume");
+    var resume = document.querySelector(".resume");
+    var form = document.querySelector("form");
+    var download = document.querySelector("#download");
+    generateBtn === null || generateBtn === void 0 ? void 0 : generateBtn.addEventListener("click", function (e) {
+        e.preventDefault();
+        if (userName && userNameResume) {
+            userNameResume.innerHTML = "Name: ".concat(userName.value);
+        }
+        if (userContact && userContactResume) {
+            console.log("Contact: ".concat(userContact.value));
+            userContactResume.innerHTML = "Contact: ".concat(userContact.value);
+        }
+        if (userEmail && userEmailResume) {
+            console.log("Email: ".concat(userEmail.value));
+            userEmailResume.innerHTML = "Email: ".concat(userEmail.value);
+        }
+        if (userEducation && userEducationResume) {
+            console.log("Education: ".concat(userEducation.value));
+            userEducationResume.innerHTML = "Education: ".concat(userEducation.value);
+        }
+        if (userExperience && userExperienceResume) {
+            console.log("Experience: ".concat(userExperience.value));
+            userExperienceResume.innerHTML = "Experience: ".concat(userExperience.value);
+        }
+        if (userSkills && userSkillsResume) {
+            console.log("Skills: ".concat(userSkills.value));
+            userSkillsResume.innerHTML = "Skills: ".concat(userSkills.value);
+        }
+        userContact.value = "";
+        userEducation.value = "";
+        userEmail.value = "";
+        userName.value = "";
+        userSkills.value = "";
+        userExperience.value = "";
+        resume.style.display = "block";
+        form.style.display = "none";
+        download.style.display = "block";
+    });
+});
+var editCode = document.querySelectorAll(".detail");
+editCode.forEach(function (element) {
+    element.addEventListener("click", function () {
+        var edit = prompt("Edit Value");
+        element.innerHTML = edit;
+    });
+});
+function download() {
+    var elements = document.querySelector(".resume");
+    html2pdf()
+        .from(elements)
+        .save();
+}
--- src/script.ts
+++ src/script.ts
@@ -13,4 +13,81 @@ skillsBtn?.addEventListener("click", function(){
   showSkills = true;
   }
   
+});
+document.addEventListener("DOMContentLoaded", () => {
+  const userName = document.querySelector("#name") as HTMLInputElement;
+  const userEmail = document.querySelector("#email") as HTMLInputElement;
+  const userContact = document.querySelector("#number") as HTMLInputElement;
+  const userExperience = document.querySelector("#experience") as HTMLInputElement;
+  const userEducation = document.querySelector("#education") as HTMLInputElement;
+  const userSkills = document.querySelector("#skills") as HTMLInputElement;
+  const generateBtn = document.querySelector("button") as HTMLButtonElement;
+
+  const userNameResume = document.querySelector("#userNameresume");
+  const userContactResume = document.querySelector("#userContactresume");
+  const userEmailResume = document.querySelector("#userEmailresume");
+  const userEducationResume = document.querySelector("#userEducationresume");
+  const userExperienceResume = document.querySelector("#userExperienceresume");
+  const userSkillsResume = document.querySelector("#userSkillsresume");
+  const resume:any = document.querySelector(".resume")
+  const form :any = document.querySelector("form")
+  const download:any = document.querySelector("#download")
+  
+
+
+  
+  generateBtn?.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    
+
+    if (userName && userNameResume) {
+      userNameResume.innerHTML = `Name: ${userName.value}`;
+    }
+    if (userContact && userContactResume) {
+      console.log(`Contact: ${userContact.value}`);
+      userContactResume.innerHTML = `Contact: ${userContact.value}`;
+    }
+    if (userEmail && userEmailResume) {
+      console.log(`Email: ${userEmail.value}`);
+      userEmailResume.innerHTML = `Email: ${userEmail.value}`;
+    }
+    if (userEducation && userEducationResume) {
+      console.log(`Education: ${userEducation.value}`);
+      userEducationResume.innerHTML = `Education: ${userEducation.value}`;
+    }
+    if (userExperience && userExperienceResume) {
+      console.log(`Experience: ${userExperience.value}`);
+      userExperienceResume.innerHTML = `Experience: ${userExperience.value}`;
+    }
+    if (userSkills && userSkillsResume) {
+      console.log(`Skills: ${userSkills.value}`);
+      userSkillsResume.innerHTML = `Skills: ${userSkills.value}`;
+    }
+    userContact.value = "";
+    userEducation.value = "";
+    userEmail.value = "";
+    userName.value = "";
+    userSkills.value = "";
+    userExperience.value = "";
+    resume.style.display = "block" 
+    form.style.display = "none";
+    download.style.display = "block";
+
+  });
+});
+const editCode:any = document.querySelectorAll(".detail")
+
+editCode.forEach((element) => {
+element.addEventListener("click", function(){
+  let edit = prompt("Edit Value");
+  element.innerHTML = edit;
 })
+})
+
+function download(){
+  const elements = document.querySelector(".resume");
+  html2pdf()
+  .from(elements)
+  .save()
+}
